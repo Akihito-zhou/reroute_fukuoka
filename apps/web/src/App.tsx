c7cbfeded61c4065f02316af612ecf3a55525815
Akihito-zhou <zhoubean0@gmail.com>
2025-11-07 11:26:09 +0900

    Delete error profile

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ChallengeDetailPage from './pages/ChallengeDetailPage';
 import HomePage from './pages/HomePage';
+import MainVisualMockPage from './pages/MainVisualMockPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/challenge/:challengeId" element={<ChallengeDetailPage />} />
+      <Route path="/mock/main-visual" element={<MainVisualMockPage />} />
     </Routes>
   </BrowserRouter>
 );
